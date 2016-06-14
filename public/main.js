9fb18f73645c9d9d2023b15bc2bb09af9e9b33bc
yarostbaklajana <yarostbaklajana@gmail.com>
2016-06-14 16:47:19 +0300

    added if case to parse comment-button mouseclick

--- public/main.js
+++ public/main.js
@@ -1,19 +1,29 @@
-var listElem = document.getElementById('file_container');
+(function () {
+    var container = document.getElementById('main');
+    var modalWindow = document.getElementById('modal-window');
+    var fadingLayout = document.getElementById('fading');
 
-function getList() {
-    listElem.addEventListener('click', function(e) {
-        if(e.target.hasAttribute('data-path') == false) {
-            return;
-        }
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                document.body.innerHTML = xhttp.responseText;
+    function getList() {
+        container.addEventListener('click', function(e) {
+            if(e.target.hasAttribute('data-path') == true) {
+                var xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function() {
+                    if (xhttp.readyState == 4 && xhttp.status == 200) {
+                        container.innerHTML = xhttp.responseText;
+                    }
+                };
+                xhttp.open("GET", "/getList?path=" + e.target.dataset.path, true );
+                xhttp.send();
+            } else if (e.target.hasAttribute('data-comment')){
+                modalWindow.style.display = 'block';
+                fadingLayout.style.display = 'block';
+                fadingLayout.style.height = container.clientHeight + 'px';
+            } else {
+                return;
             }
-        };
-        xhttp.open("GET", "/getList?path=" + e.target.dataset.path, true );
-        xhttp.send();
-    });
-}
+        });
+    }
 
-getList();
+    getList();
+})();
+
