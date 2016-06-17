1e511ab1191a20a65a8a2ab302e217ff6ce090d0
yarostbaklajana <yarostbaklajana@gmail.com>
2016-06-17 11:57:34 +0300

    changed maim module considering single responsibility principle

--- public/main.js
+++ public/main.js
@@ -1,29 +1,29 @@
-(function () {
+(function (commentModal) {
     var container = document.getElementById('main');
-    var modalWindow = document.getElementById('modal-window');
-    var fadingLayout = document.getElementById('fading');
 
-    function getList() {
+    function addClickHandlerToContainer() {
         container.addEventListener('click', function(e) {
             if(e.target.hasAttribute('data-path') == true) {
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function() {
-                    if (xhttp.readyState == 4 && xhttp.status == 200) {
-                        container.innerHTML = xhttp.responseText;
-                    }
-                };
-                xhttp.open("GET", "/getList?path=" + e.target.dataset.path, true );
-                xhttp.send();
-            } else if (e.target.hasAttribute('data-comment')){
-                modalWindow.style.display = 'block';
-                fadingLayout.style.display = 'block';
-                fadingLayout.style.height = container.clientHeight + 'px';
+                getList(e.target);
+            } else if (e.target.hasAttribute('data-commented')){
+                commentModal.showModal(e.target);
             } else {
                 return;
             }
         });
     }
 
-    getList();
-})();
+    function getList(target) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                container.innerHTML = xhttp.responseText;
+            }
+        };
+        xhttp.open("GET", "/getList?path=" + target.dataset.path, true );
+        xhttp.send();
+    }
+
+    addClickHandlerToContainer();
+})(window.commentModal);
 
