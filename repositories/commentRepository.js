07492c2e0ea4ce259ad9890fd45fe476562ae8f4
yarostbaklajana <yarostbaklajana@gmail.com>
2016-07-13 11:07:29 +0300

    added functionality to commentRepository

--- repositories/commentRepository.js
+++ repositories/commentRepository.js
@@ -1,9 +1,13 @@
+var comments = [];
+
 module.exports = {
-    getComments: function(path) {
-        
+    getComments: function(path, callback) {
+        var commentsForFile = comments.filter(function(item) { return item.path == path });
+        callback(null, comments);
     },
-    saveComment: function(comment) {
-        
+    saveComment: function(comment, callback) {
+        comments.push(comment);
+        callback(null);
     }
 }
 
