b043bdfe086528381ab7499b625285d5ddb4115a
yarostbaklajana <yarostbaklajana@gmail.com>
2016-07-15 12:15:43 +0300

    added post method for saving comments to db

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ var ejs = require('ejs');
 var dirreader = require('./libs/dirreader');
 var root = require('./appConstants/rootConstant').ROOT;
 var commentRepository = require('./repositories/commentRepository');
+var bodyParser = require('body-parser');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(root));
@@ -30,6 +34,20 @@ router.get('/getList', function(req, res) {
 router.get('/getComments', function(req, res) {
   commentRepository.getComments(req.query.path, function (err, comments) {
     res.render('comments', {fileComments: comments});
+  });
+});
+
+router.post('/saveUserComment', function (req, res) {
+  var comment = {
+    path: req.body.path,
+    username: req.body.username,
+    comment: req.body.comment
+  }
+
+  commentRepository.saveComment(comment, function (err) {
+    commentRepository.getComments(comment.path, function (err, comments) {
+      res.render('comments', {fileComments: comments});
+    });
   });
 });
 
